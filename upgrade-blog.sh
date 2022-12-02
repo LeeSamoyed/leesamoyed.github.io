35a9c6bd4ca7e57b4aa11905f3f596047ed4e46a
Yi Li <1528289501@qq.com>
2022-12-02 11:21:06 +0000

    path

--- upgrade-blog.sh
+++ upgrade-blog.sh
@@ -1,9 +1,4 @@
 #!/bin/bash
 mkdocs build --clean
-cp -r site/assets/javascripts ./assets
-cp -r site/assets/stylesheets ./assets
-rm -rf site/assets/
-rm -rf site/index.html
-rm -rf site/404.html
 cp -r site/* ./
 rm -rf site
