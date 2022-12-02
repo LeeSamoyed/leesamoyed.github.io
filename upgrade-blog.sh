be4817f17ab202015cad8c80e4ed37a2287af746
Yi Li <1528289501@qq.com>
2022-12-02 02:58:54 +0000

    sh

--- upgrade-blog.sh
+++ upgrade-blog.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 mkdocs build --clean
-cp -r material/assets/javascripts ./assets/javascripts
-cp -r material/assets/stylesheets ./assets/stylesheets
+cp -r material/assets/javascripts ./assets
+cp -r material/assets/stylesheets ./assets
 rm -rf site/assets/
 rm -rf site/index.html
 rm -rf site/404.html
