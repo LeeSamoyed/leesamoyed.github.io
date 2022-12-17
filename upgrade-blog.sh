32c35bc00ee1cb382d8d2785e8fb5eb3698d02d6
Yi Li <1528289501@qq.com>
2022-12-17 02:47:48 +0000

    gitee

--- upgrade-blog.sh
+++ upgrade-blog.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 mkdocs build --clean
-rm -rf $(ls | grep -v docs | grep -v material | grep -v site | grep -v .gitignore | grep -v gitpod.yml | grep -v mkdocs.yml | grep -v README.md | grep -v upgrade-blog.sh)
+rm -rf $(ls | grep -v docs | grep -v material | grep -v  .github | grep -v site | grep -v .gitignore | grep -v gitpod.yml | grep -v mkdocs.yml | grep -v README.md | grep -v upgrade-blog.sh)
 mv site/* ./
