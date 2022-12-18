06e52db8e3f57400ac33fab61168efbd0e2f847a
Yi Li <1528289501@qq.com>
2022-12-18 10:09:35 +0000

    repo-mirror

--- upgrade-blog.sh
+++ upgrade-blog.sh
@@ -13,7 +13,7 @@ if [ $# -gt 0 ]
   rm -rf $(ls | grep -v docs | grep -v material | \
           grep -v  .github | grep -v site | grep -v .gitignore | \
           grep -v gitpod.yml | grep -v mkdocs.yml | grep -v README.md | \
-          grep -v upgrade-blog.sh | grep -v git-mirror.sh | grep -v entrypoint.sh | \
+          grep -v upgrade-blog.sh | grep -v repo-mirror.sh | grep -v entrypoint.sh | \
           grep -v Dockerfile | grep -v action.yml)
   mv site/* ./
 
