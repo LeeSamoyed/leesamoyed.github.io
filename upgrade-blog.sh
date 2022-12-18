c6b338828c971731855f2f4851de28a4ecc654bc
Yi Li <1528289501@qq.com>
2022-12-18 10:03:37 +0000

    repo-mirror

--- upgrade-blog.sh
+++ upgrade-blog.sh
@@ -3,6 +3,7 @@
 if [ $# -le 0 ]
   then
   echo what is your commmit notes?
+  exit
 fi
 
 if [ $# -gt 0 ]
@@ -12,9 +13,8 @@ if [ $# -gt 0 ]
   rm -rf $(ls | grep -v docs | grep -v material | \
           grep -v  .github | grep -v site | grep -v .gitignore | \
           grep -v gitpod.yml | grep -v mkdocs.yml | grep -v README.md | \
-          grep -v upgrade-blog.sh)
-          # | grep -v git-mirror.sh | grep -v entrypoint.sh | \
-          #grep -v Dockerfile | grep -v action.yml)
+          grep -v upgrade-blog.sh | grep -v git-mirror.sh | grep -v entrypoint.sh | \
+          grep -v Dockerfile | grep -v action.yml)
   mv site/* ./
 
   # auto push to github
