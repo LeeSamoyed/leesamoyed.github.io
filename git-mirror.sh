3cc37ab4e8c3688ff4b4c2722ec5e386c130174f
Yi Li <1528289501@qq.com>
2022-12-18 05:28:20 +0000

    repo2gitee

--- git-mirror.sh
+++ git-mirror.sh
@@ -13,8 +13,6 @@ echo "SOURCE=$SOURCE_REPO"
 echo "DESTINATION=$DESTINATION_REPO"
 echo "DRY RUN=$DRY_RUN"
 
-# git config --global --add safe.directory /github/workspace
-
 git clone --mirror "$SOURCE_REPO" "$SOURCE_DIR" && cd "$SOURCE_DIR"
 git remote set-url --push origin "$DESTINATION_REPO"
 git fetch -p origin
--- upgrade-blog.sh
+++ upgrade-blog.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
 mkdocs build --clean
-rm -rf $(ls | grep -v docs | grep -v material | grep -v  .github | grep -v site | grep -v .gitignore | grep -v gitpod.yml | grep -v mkdocs.yml | grep -v README.md | grep -v upgrade-blog.sh)
+rm -rf $(ls | grep -v docs | grep -v material | \
+        grep -v  .github | grep -v site | grep -v .gitignore | \
+        grep -v gitpod.yml | grep -v mkdocs.yml | grep -v README.md | \
+        grep -v upgrade-blog.sh | grep -v git-mirror.sh | grep -v entrypoint.sh | \
+        grep -v Dockerfile | grep -v action.yml)
 mv site/* ./
