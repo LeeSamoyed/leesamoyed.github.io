d74ec2663421cb71a0ca8b43d674c1f609142c59
LeeSamoyed <41322950+LeeSamoyed@users.noreply.github.com>
2023-03-19 18:14:53 +0800

    upgrade-sh

--- upgrade-blog.sh
+++ upgrade-blog.sh
@@ -10,12 +10,13 @@ fi
 # update wiki 
 if [ $# -gt 0 ]
   then
-  rmfile=`ls ./ | grep -v .docs | grep -v material | \
-          grep -v  .github | grep -v site | grep -v .gitignore | \
-          grep -v gitpod.yml | grep -v mkdocs.yml | grep -v README.md | \
-          grep -v upgrade-blog.sh | grep -v .repo-mirror | \
-          grep -v Dockerfile | grep -v action.yml`
-  echo "***These files will be delelte:***\n"${rmfile}"\n***Input 'y' to continue upgrade and others to stop***"
+  # rmfile=`ls ./ | grep -v .docs | grep -v material | \
+  #         grep -v  .github | grep -v site | grep -v .gitignore | \
+  #         grep -v gitpod.yml | grep -v mkdocs.yml | grep -v README.md | \
+  #         grep -v upgrade-blog.sh | grep -v .repo-mirror | \
+  #         grep -v Dockerfile | grep -v action.yml`
+  # echo "***These files will be delelte:***\n"${rmfile}"\n***Input 'y' to continue upgrade and others to stop***"
+  echo "***Are U sure to upgrade***\n"${rmfile}"***Input 'y' to continue upgrade and others to stop***"
   
   read makesure
   if [ $makesure == "y" ]
@@ -23,14 +24,14 @@ if [ $# -gt 0 ]
   # clean & delete
   mkdocs build --clean
   echo "***Build success***"
-  rm -rf $(ls ./ | grep -v .docs | grep -v material | \
-          grep -v  .github | grep -v site | grep -v .gitignore | \
-          grep -v gitpod.yml | grep -v mkdocs.yml | grep -v README.md | \
-          grep -v upgrade-blog.sh | grep -v .repo-mirror | \
-          grep -v Dockerfile | grep -v action.yml)
-  echo "***Delete success***"
-  mv ./site/*  ./
-  echo "***Move success***"
+  # rm -rf $(ls ./ | grep -v .docs | grep -v material | \
+  #         grep -v  .github | grep -v site | grep -v .gitignore | \
+  #         grep -v gitpod.yml | grep -v mkdocs.yml | grep -v README.md | \
+  #         grep -v upgrade-blog.sh | grep -v .repo-mirror | \
+  #         grep -v Dockerfile | grep -v action.yml)
+  # echo "***Delete success***"
+  cp -r ./site/*  ./
+  echo "***Copy success***"
 
   # auto push to github
   git add .
@@ -41,10 +42,3 @@ if [ $# -gt 0 ]
     echo "***Exit***"
   fi
 fi
-
-
-
-
-
-
-  
