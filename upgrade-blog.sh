d996937af5aa3822342ca091c1d65153239a2b89
LeeSamoyed <41322950+LeeSamoyed@users.noreply.github.com>
2023-03-24 18:29:43 +0800

    upgrade-sh

--- upgrade-blog.sh
+++ upgrade-blog.sh
@@ -10,36 +10,34 @@ fi
 # update wiki 
 if [ $# -gt 0 ]
   then
-  rmfile=`ls ./ | grep -v .docs | grep -v material | \
-          grep -v  .github | grep -v site | grep -v .gitignore | \
-          grep -v gitpod.yml | grep -v mkdocs.yml | grep -v README.md | \
-          grep -v upgrade-blog.sh | grep -v .repo-mirror | \
-          grep -v Dockerfile | grep -v action.yml`
-  echo "***These files will be overload:***\n"${rmfile}"\n***Input 'y' to continue upgrade and others to stop***"
-  # echo "***Are U sure to upgrade***\n"${rmfile}"***Input 'y' to continue upgrade and others to stop***"
-  
-  read makesure
-  if [ $makesure == "y" ]
-  then
-  # clean & delete
+
+  # build
   mkdocs build --clean
   echo "***Build success***"
-  # rm -rf $(ls ./ | grep -v .docs | grep -v material | \
-  #         grep -v  .github | grep -v site | grep -v .gitignore | \
-  #         grep -v gitpod.yml | grep -v mkdocs.yml | grep -v README.md | \
-  #         grep -v upgrade-blog.sh | grep -v .repo-mirror | \
-  #         grep -v Dockerfile | grep -v action.yml)
+
+  # clean & delete
+  origin=./site   #文件夹a
+  target=./
+  for i in `ls $origin`
+  do
+  echo $i
+  rm -rf $target/$i 
+  done
   echo "***Delete success***"
-  cp -rf ./site/*  ./
-  echo "***Copy success***"
-  rm -rf ls ./site/*
+
+  mv ./site/*  ./
+  echo "***Remove success***"
+  rm -rf ./site
 
   # auto push to github
   git add .
   git commit -m $1
   git push origin master
   echo "***Upgrade success***"
-  else
-    echo "***Exit***"
-  fi
 fi
+
+# rm -rf $(ls ./ | grep -v .docs | grep -v material | \
+#         grep -v  .github | grep -v site | grep -v .gitignore | \
+#         grep -v gitpod.yml | grep -v mkdocs.yml | grep -v README.md | \
+#         grep -v upgrade-blog.sh | grep -v .repo-mirror | \
+#         grep -v Dockerfile | grep -v action.yml)
